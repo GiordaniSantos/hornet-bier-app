7e56f3dbee1ed64c38287001b54c791acd6aba97
Giordani <dani.santossilveira@hotmail.com>
2024-12-25 11:09:25 -0300

    ajustes iniciais

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Drawer } from 'expo-router/drawer';
+import { Link } from 'expo-router';
+import { HeaderButton } from '../../components/HeaderButton';
+
+const DrawerLayout = () => (
+  <Drawer>
+    <Drawer.Screen
+      name="index"
+      options={{
+        headerTitle: 'Home',
+        drawerLabel: 'Home',
+        drawerIcon: ({ size, color }) => <Ionicons name="home-outline" size={size} color={color} />,
+        headerRight: () => (
+          <Link href="/modal" asChild>
+            <HeaderButton />
+          </Link>
+        ),
+      }}
+    />
+  </Drawer>
+);
+
+export default DrawerLayout;
