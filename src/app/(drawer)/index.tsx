15f941cecafa0934011ae4b10b1691de45480ea8
Giordani <dani.santossilveira@hotmail.com>
2024-12-26 18:47:00 -0300

    ajustes estilo os

--- src/app/(drawer)/index.tsx
+++ src/app/(drawer)/index.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Container } from '@/src/components/Container';
-import { ScreenContent } from '@/src/components/ScreenContent';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function Home() {
   return (
@@ -15,15 +15,26 @@ export default function Home() {
                 <Text style={styles.title}>131.26.2024</Text>
                 <Text style={styles.badgeOpen}>DublinBier</Text>
               </View>
-              <Text style={styles.badgeApproved}>ABERTO</Text>
+              <Text style={[styles.badgeApproved, styles.colorApproved]}>ABERTO</Text>
             </View>
             <Text style={styles.author}>Valor total: R$280,00</Text>
             <Text style={styles.date}>
               Entrada em: 23/12/2024 | Saída: 24/12/2024
             </Text>
-            <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
-              <Text style={styles.buttonText}>Saiba Mais</Text>
-            </TouchableOpacity>
+            <View style={styles.containerButtons}>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="edit" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="eye" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="trash" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="whatsapp" size={14} color={'#000'} />
+              </TouchableOpacity>
+            </View>
           </View>
           <View style={styles.card}>
             <View style={styles.header}>
@@ -31,15 +42,26 @@ export default function Home() {
                 <Text style={styles.title}>129.35.2024</Text>
                 <Text style={styles.badgeOpen}>Indústria e Comércio de Chopeiras Ribeirão Preto ( MEMO )</Text>
               </View>
-              <Text style={styles.badgeApproved}>FECHADO</Text>
+              <Text style={[styles.badgeApproved, styles.colorClosed]}>FECHADO</Text>
             </View>
             <Text style={styles.author}>Valor total: R$280,00</Text>
             <Text style={styles.date}>
               Entrada em: 23/12/2024 | Saída: 24/12/2024
             </Text>
-            <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
-              <Text style={styles.buttonText}>Saiba Mais</Text>
-            </TouchableOpacity>
+            <View style={styles.containerButtons}>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="edit" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="eye" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="trash" size={14} color={'#000'} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {/* Navigate to project */}}>
+                <FontAwesome5 name="whatsapp" size={14} color={'#000'} />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Container>
@@ -53,7 +75,7 @@ const styles = StyleSheet.create({
   },
   card: {
     borderRadius: 15,
-    height: 280,
+    height: 210,
     overflow: 'hidden',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
@@ -92,8 +114,13 @@ const styles = StyleSheet.create({
     padding: 4,
     borderRadius: 8,
   },
+  colorApproved: {
+    backgroundColor: '#28a745'
+  },
+  colorClosed: {
+    backgroundColor: '#ad3a13'
+  },
   badgeApproved: {
-    backgroundColor: '#28a745',
     color: '#fff',
     fontSize: 14,
     padding: 5,
@@ -109,10 +136,15 @@ const styles = StyleSheet.create({
     color: '#6c757d',
     fontSize: 14,
   },
+  containerButtons: {
+    display: 'flex', 
+    flexDirection: 'row'
+  },
   button: {
-    marginTop: 16,
+    marginTop: 20,
+    marginRight: 10,
     backgroundColor: 'transparent',
-    borderColor: '#28a745',
+    borderColor: '#ff3a00',
     borderWidth: 1,
     borderRadius: 8,
     paddingVertical: 10,
@@ -120,7 +152,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   buttonText: {
-    color: '#28a745',
+    color: '#000',
     fontSize: 16,
   },
 });
