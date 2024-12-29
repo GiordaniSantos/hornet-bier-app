ad3dc3ebdac2e3f54af619c5d16536637d939ab7
Giordani <dani.santossilveira@hotmail.com>
2024-12-29 16:05:58 -0300

    ajustes formulario criar cliente

--- src/app/(drawer)/clientes/create.tsx
+++ src/app/(drawer)/clientes/create.tsx
@@ -1,11 +1,17 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
 import { Container } from '@/src/components/Container';
-import { useForm, Controller } from "react-hook-form"
+import { useForm, Controller } from "react-hook-form";
 
 type formData = {
   nome: string;
-  nomeContato: string
+  nomeContato: string;
+  email: string;
+  cpfCnpj: string;
+  cidade: string;
+  celular: string;
+  celularSecundario: string;
+  telefone: string;
 }
 
 export default function CreateOS() {
@@ -13,22 +19,31 @@ export default function CreateOS() {
     defaultValues: {
       nome: "",
       nomeContato: "",
+      email: "",
+      cpfCnpj: "",
+      cidade: "",
+      celular: "",
+      celularSecundario: "",
+      telefone: ""
     },
   })
 
-  const onSubmit = (data: formData) => console.log(data)
+  const onSubmit = (data: formData) => {
+    console.log(data)
+  }
 
   return (
     <>
       <Stack.Screen options={{ title: 'Criar Cliente' }} />
       <Container>
-          <View style={styles.container}>
+        <View style={styles.container}>
+          <ScrollView>
             <View style={styles.containerTable}>
-              <ScrollView>
                 <Controller
                   control={control}
                   rules={{
                     required: true,
+                    maxLength: 250
                   }}
                   render={({ field: { onChange, onBlur, value } }) => (
                     <>
@@ -39,16 +54,16 @@ export default function CreateOS() {
                         onChangeText={onChange}
                         value={value}
                       />
+                      {errors.nome && errors.nome.type == "required" && <Text style={styles.textError}>Preencha este campo!</Text>}
+                      {errors.nome && errors.nome.type == "maxLength" && <Text style={styles.textError}>No máximo 250 caracteres!</Text>}
                     </>
                   )}
                   name="nome"
                 />
-                {errors.nome && <Text>This is required.</Text>}
-
                 <Controller
                   control={control}
                   rules={{
-                    maxLength: 100,
+                    maxLength: 250,
                   }}
                   render={({ field: { onChange, onBlur, value } }) => (
                     <>
@@ -59,16 +74,119 @@ export default function CreateOS() {
                         onChangeText={onChange}
                         value={value}
                       />
+                      {errors.nomeContato && <Text style={styles.textError}>No máximo 250 caracteres!</Text>}
                     </>
                   )}
                   name="nomeContato"
                 />
+                <Controller
+                  control={control}
+                  rules={{
+                    maxLength: 100,
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "Digite um email válido!"
+                    }
+                  }}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label]}>Email</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                      {errors.email && <Text style={styles.textError}>Digite um email válido!</Text>}
+                    </>
+                  )}
+                  name="email"
+                />
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label, {width: 80}]}>CPF/CNPJ</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    </>
+                  )}
+                  name="cpfCnpj"
+                />
+                <Controller
+                  control={control}
+                  rules={{
+                    maxLength: 200,
+                  }}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label, {width: 60}]}>Cidade</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                      {errors.cidade && <Text style={styles.textError}>No máximo 200 caracteres!</Text>}
+                    </>
+                  )}
+                  name="cidade"
+                />
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label, {width: 125}]}>Celular Principal</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    </>
+                  )}
+                  name="celular"
+                />
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label, {width: 140}]}>Celular Secundário</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    </>
+                  )}
+                  name="celularSecundario"
+                />
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <>
+                      <Text style={[styles.label, {width: 70}]}>Telefone</Text>
+                      <TextInput
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    </>
+                  )}
+                  name="telefone"
+                />
                 <View style={styles.buttonSave}>
-                  <Button title="Cadastrar" color={'#0f5d39'} onPress={handleSubmit(onSubmit)} />
+                  <Button title="Cadastrar" color={'#1cc88a'} onPress={handleSubmit(onSubmit)} />
                 </View>
-              </ScrollView>
             </View>
-          </View>
+          </ScrollView>
+        </View>
       </Container>
     </>
   );
@@ -93,7 +211,12 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ccc',
   },
   buttonSave: {
-    marginTop: 15,
+    marginTop: 5,
+  },
+  textError: {
+    marginTop: -15,
+    marginBottom: 8,
+    color: '#fc0303'
   },
   label: {
     fontSize: 16,
